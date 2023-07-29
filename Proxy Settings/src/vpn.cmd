73ebb5a5c24dd34a18cd7d05212f36aa83c5d760
Masih <56260232+Masihbr@users.noreply.github.com>
2023-07-29 16:20:24 +0330

    Use one Ctrl-C to quit

--- Proxy Settings/src/vpn.cmd
+++ Proxy Settings/src/vpn.cmd
@@ -1,6 +1,12 @@
 @echo off
-call "%USERPROFILE%\Proxy Settings\config.cmd"
-call "%USERPROFILE%\Proxy Settings\proxy_on.cmd"
-stnlc -unat %proxy_username%@%host_ip%:%host_port% -pw=%proxy_password% -proxyListPort=%proxy_port% -proxyFwding=y
-call "%USERPROFILE%\Proxy Settings\proxy_off.cmd"
 
+call "%USERPROFILE%\Proxy Settings\config.cmd"
+
+if "%~1" neq "_start_" (
+  call "%USERPROFILE%\Proxy Settings\proxy_on.cmd"
+  stnlc -unat %proxy_username%@%host_ip%:%host_port% -pw=%proxy_password% -proxyListPort=%proxy_port% -proxyFwding=y
+  cmd /c "%~f0" _start_ %*
+  call "%USERPROFILE%\Proxy Settings\proxy_off.cmd"
+  exit /b
+)
+shift /1
