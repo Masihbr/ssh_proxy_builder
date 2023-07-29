f9d0c4577891284570187e2a61381fd65ec22a63
Masih <56260232+Masihbr@users.noreply.github.com>
2023-07-29 17:13:51 +0330

    Remove unnecessary timeout

--- Proxy Settings/src/vpn.cmd
+++ Proxy Settings/src/vpn.cmd
@@ -9,7 +9,6 @@ if "%~1" neq "_start_" (
   stnlc -unat %proxy_username%@%host_ip%:%host_port% -pw=%proxy_password% -proxyListPort=%proxy_port% -proxyFwding=y | find "ERROR" && timeout 3 && goto loop
   cmd /c "%~f0" _start_ %*
   call "%USERPROFILE%\Proxy Settings\proxy_off.cmd"
-  timeout 3
   exit /b
 )
 shift /1
