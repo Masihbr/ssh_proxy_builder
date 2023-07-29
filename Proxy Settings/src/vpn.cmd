88533505bbee8dc556bb8c2e6da2a471dac3b9df
Masih <56260232+Masihbr@users.noreply.github.com>
2023-07-29 17:13:06 +0330

    Loop forever and try to reconnect
    Ctrl-C means exit and any error case makes stnlc loop forever

--- Proxy Settings/src/vpn.cmd
+++ Proxy Settings/src/vpn.cmd
@@ -4,9 +4,12 @@ call "%USERPROFILE%\Proxy Settings\config.cmd"
 
 if "%~1" neq "_start_" (
   call "%USERPROFILE%\Proxy Settings\proxy_on.cmd"
-  stnlc -unat %proxy_username%@%host_ip%:%host_port% -pw=%proxy_password% -proxyListPort=%proxy_port% -proxyFwding=y
+  :loop
+  echo CONNECTED...
+  stnlc -unat %proxy_username%@%host_ip%:%host_port% -pw=%proxy_password% -proxyListPort=%proxy_port% -proxyFwding=y | find "ERROR" && timeout 3 && goto loop
   cmd /c "%~f0" _start_ %*
   call "%USERPROFILE%\Proxy Settings\proxy_off.cmd"
+  timeout 3
   exit /b
 )
 shift /1
