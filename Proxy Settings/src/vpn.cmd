ec38ace4dbf0407e173b74baa9b5ccdaa3a56c46
Masih <56260232+Masihbr@users.noreply.github.com>
2023-08-23 11:55:58 +0330

    Remove exponential timeout

--- Proxy Settings/src/vpn.cmd
+++ Proxy Settings/src/vpn.cmd
@@ -13,12 +13,12 @@ call "%USERPROFILE%\Proxy Settings\config.cmd"
 
 if "%~1" neq "_start_" (
   call "%USERPROFILE%\Proxy Settings\proxy_on.cmd"
-  set "wait_time=2"
+  set "wait_time=3"
   :loop
   echo CONNECTED...
   stnlc -unat %proxy_username%@%host_ip%:%host_port% -pw=%proxy_password% -proxyListPort=%proxy_port% -proxyFwding=y | find "ERROR" && (
     timeout %wait_time%
-    set /A "wait_time*=2"
+    set /A "wait_time=3"
     goto loop
   )
   cmd /c "%~f0" _start_ %*
