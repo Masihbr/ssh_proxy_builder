f44484e7f517c1d0cdf2b3cf8ec602a09b7d905c
Masih <56260232+Masihbr@users.noreply.github.com>
2023-08-23 11:29:48 +0330

    Update vpn.cmd

--- Proxy Settings/src/vpn.cmd
+++ Proxy Settings/src/vpn.cmd
@@ -2,6 +2,13 @@
 
 call "%USERPROFILE%\Proxy Settings\config.cmd"
 
+if exist stnlc (
+echo stnlc is okay!
+) else (
+echo YOU NEED TO DOWNLOAD AND INSTALL "bitvise ssh client" THEN RUN THE SCRIPT AGAIN!
+exit /b
+)
+
 if "%~1" neq "_start_" (
   call "%USERPROFILE%\Proxy Settings\proxy_on.cmd"
   :loop
