00ef869c58a0401bc32545e350ce3d6e27d177ab
Masih <56260232+Masihbr@users.noreply.github.com>
2023-08-23 11:48:36 +0330

    Print error if stnlc is not installed

--- Proxy Settings/src/vpn.cmd
+++ Proxy Settings/src/vpn.cmd
@@ -1,16 +1,15 @@
 @echo off
 
-call "%USERPROFILE%\Proxy Settings\config.cmd"
-
-if exist stnlc (
-echo stnlc is okay!
+where stnlc >nul 2>&1
+if %errorlevel% equ 0 (
+    echo stnlc is okay!
 ) else (
-echo YOU NEED TO DOWNLOAD AND INSTALL "bitvise ssh client" THEN RUN THE SCRIPT AGAIN!
-@pause
-exit /b
+    echo YOU NEED TO DOWNLOAD AND INSTALL "bitvise ssh client" THEN RUN THE SCRIPT AGAIN!  
+    @pause
+    exit /b
 )
 
-
+call "%USERPROFILE%\Proxy Settings\config.cmd"
 
 if "%~1" neq "_start_" (
   call "%USERPROFILE%\Proxy Settings\proxy_on.cmd"
