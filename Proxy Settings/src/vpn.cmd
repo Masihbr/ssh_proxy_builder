2f86410d597325a96b76c016171fc3102628e45c
Masih <56260232+Masihbr@users.noreply.github.com>
2023-08-23 11:32:03 +0330

    Add exponential wait

--- Proxy Settings/src/vpn.cmd
+++ Proxy Settings/src/vpn.cmd
@@ -9,11 +9,18 @@ echo YOU NEED TO DOWNLOAD AND INSTALL "bitvise ssh client" THEN RUN THE SCRIPT A
 exit /b
 )
 
+
+
 if "%~1" neq "_start_" (
   call "%USERPROFILE%\Proxy Settings\proxy_on.cmd"
+  set "wait_time=2"
   :loop
   echo CONNECTED...
-  stnlc -unat %proxy_username%@%host_ip%:%host_port% -pw=%proxy_password% -proxyListPort=%proxy_port% -proxyFwding=y | find "ERROR" && timeout 3 && goto loop
+  stnlc -unat %proxy_username%@%host_ip%:%host_port% -pw=%proxy_password% -proxyListPort=%proxy_port% -proxyFwding=y | find "ERROR" && (
+    timeout %wait_time%
+    set /A "wait_time*=2"
+    goto loop
+  )
   cmd /c "%~f0" _start_ %*
   call "%USERPROFILE%\Proxy Settings\proxy_off.cmd"
   exit /b
